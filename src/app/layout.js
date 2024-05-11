e771f22f5fff3e42459ecad8834e5bf097097666
jhtl08 <jason.lorenzo@student.ateneo.edu>
2024-05-11 11:01:27 +0800

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
